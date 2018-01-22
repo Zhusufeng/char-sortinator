8b457e80c51889baa9287a88d42918bc708a1e7c
Lisa Gee <zhusufeng@gmail.com>
2018-01-22 14:16:05 -0800

    Add post func to endpt /sort

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.static(__dirname + '/client'));
+
+const tempStorage = [];
+
+app.post('/sort', (req, res) => {
+  console.log(req.body); // 'cba';
+
+  const sortedBody = req.body.sort();
+  tempStorage.push(sortedBody);
+
+  res.status(201).send(sortedBody);
+});
 
 app.listen(8123, () => {
   console.log('App listening on port 8123');
