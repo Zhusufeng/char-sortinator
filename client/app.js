d7e13e5d8de8a0772ca0d77814adb626756acdff
Lisa Gee <zhusufeng@gmail.com>
2018-01-22 16:02:44 -0800

    Clean up app.js code, send JSON object to server

--- client/app.js
+++ client/app.js
@@ -1,20 +1,21 @@
 const sortForm = document.getElementById('sort-form');
 const input = document.getElementById('input');
-console.log('this loaded');
 
 const submitInfo = function (e) {
   e.preventDefault();
-  console.log('You pushed submit');
+
+  const data = { input: input.value };
+
   fetch('/sort', {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/json'
     }),
-    body: JSON.stringify(input)
-  }).then((data) => {
-    console.log(data.json);
+    body: JSON.stringify(data)
+  }).then((info) => {
+    console.log(info);
   });
 
-}
+};
 
 sortForm.addEventListener('submit', submitInfo);
